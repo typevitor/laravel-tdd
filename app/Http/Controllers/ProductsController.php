3ae9a29f98c25b8a5a70d39793d72a1fee597f3b
Vitor Leal <vitorlealeng@gmail.com>
2022-12-30 09:44:49 -0300

    Add products list

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('products.index', [
+            'products' => Product::query()->get()->toArray(),
+        ]);
+    }
+}
