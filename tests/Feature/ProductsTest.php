b731b21940ef08ec027bc70dd3c91b4f07522c88
Vitor A Leal <vitorlealeng@gmail.com>
2023-01-03 08:39:38 -0300

    Configure sqlite test database

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_products_list_is_empty()
     {
         $user = User::factory()->create();
@@ -20,5 +24,22 @@ class ProductsTest extends TestCase
         $response = $this->get('/products');
         $response->assertStatus(200);
         $response->assertSee(__('No products found'));
+    }
+
+    public function test_products_list_is_not_empty()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+        ->post('/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        Product::create(['name' => 'product_test', 'price' => 10]);
+
+        $response = $this->get('/products');
+        $response->assertStatus(200);
+        $response->assertDontSee(__('No products found'));
     }
 }
