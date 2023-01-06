a8d7bb72335b9ca12ee45cd053f5766aca1c76aa
Vitor A Leal <vitorlealeng@gmail.com>
2023-01-06 13:59:47 -0300

    testing protected routes

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -14,14 +14,7 @@ class ProductsTest extends TestCase
     public function test_products_list_is_empty()
     {
         $user = User::factory()->create();
-
-        $this->actingAs($user)
-        ->post('/login', [
-            'email' => 'test@example.com',
-            'password' => 'password',
-        ]);
-
-        $response = $this->get('/products');
+        $response = $this->actingAs($user)->get('/products');
         $response->assertStatus(200);
         $response->assertSee(__('No products found'));
     }
@@ -29,16 +22,8 @@ class ProductsTest extends TestCase
     public function test_products_list_is_not_empty()
     {
         $user = User::factory()->create();
-
-        $this->actingAs($user)
-        ->post('/login', [
-            'email' => 'test@example.com',
-            'password' => 'password',
-        ]);
-
         $product = Product::create(['name' => 'product_test', 'price' => 10]);
-
-        $response = $this->get('/products');
+        $response = $this->actingAs($user)->get('/products');
         $response->assertStatus(200);
         $response->assertDontSee(__('No products found'));
         $response->assertViewHas('products', function($collection) use ($product) {
