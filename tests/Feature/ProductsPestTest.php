e92d0b6044d54eb3cc467c66593b4afa803af462
Vitor Leal <vitorlealeng@gmail.com>
2023-01-14 08:26:16 -0300

    expect

--- tests/Feature/ProductsPestTest.php
+++ tests/Feature/ProductsPestTest.php
@@ -2,6 +2,7 @@
 
 beforeEach(function() {
     $this->user = createUser(isAdmin: false);
+    $this->admin = createUser(isAdmin: true);
 });
 
 test('products list is empyy', function () {
@@ -18,4 +19,22 @@ test('products list is not empyy', function () {
         ->assertViewHas('products', function($collection) use ($product) {
             return $collection->contains($product);
         });
+});
+
+test('create product successful', function() {
+    $product = [
+        'name' => 'test',
+        'price' => '1234.00'
+    ];
+    $this->actingAs($this->admin)->post('/products', $product)
+        ->assertStatus(302)
+        ->assertRedirect('/products');
+
+    $this->assertDatabaseHas('products', $product);
+
+    $lastProduct = \App\Models\Product::latest()->first();
+    //$this->assertEquals($product['name'], $lastProduct->name);
+    //$this->assertEquals($product['price'], $lastProduct->price);
+    expect($lastProduct->name)->toBe($product['name']);
+    expect($lastProduct->price)->toBe($product['price']);
 });
