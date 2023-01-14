ecdce86cf1b6756d8accb6110353fe7ebe95aa89
Vitor Leal <vitorlealeng@gmail.com>
2023-01-14 08:17:45 -0300

    Products tests with Pest

--- tests/Feature/ProductsPestTest.php
+++ tests/Feature/ProductsPestTest.php
@@ -9,3 +9,13 @@ test('products list is empyy', function () {
         ->assertStatus(200)
         ->assertSee(__('No products found'));
 });
+
+test('products list is not empyy', function () {
+    $product = \App\Models\Product::create(['name' => 'product_test', 'price' => 10]);
+    $this->actingAs($this->user)->get('/products')
+        ->assertStatus(200)
+        ->assertDontSee(__('No products found'))
+        ->assertViewHas('products', function($collection) use ($product) {
+            return $collection->contains($product);
+        });
+});
