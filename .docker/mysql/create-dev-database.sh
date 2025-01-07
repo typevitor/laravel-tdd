6128c56e89010e946b0f27b60243448c65518054
Vitor Augusto <vitorlealeng@gmail.com>
2025-01-07 09:10:45 -0300

    add entrypoint to create dev database

---
+++ .docker/mysql/create-dev-database.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo $MYSQL_DATABASE;
+
+mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+    GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE%\`.* TO '$MYSQL_USER'@'%';
+EOSQL
