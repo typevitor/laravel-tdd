2f963637bb38af1469739a9c54fde7f94b19036d
Vitor Augusto <vitorlealeng@gmail.com>
2025-01-09 09:15:07 -0300

    test: add new date range test

--- tests/Unit/Domain/ValueObjects/DateRangeTest.php
+++ tests/Unit/Domain/ValueObjects/DateRangeTest.php
@@ -22,38 +22,50 @@ it('should return an exception if end date is before start date', function () {
     })->toThrow(DateRangeException::class, 'End date must be after start date');
 });
 
-
 it('should calculate the amount of nights for the reservation', function () {
     $reservation1 = new DateRange(
-        Carbon::parse('2025-01-01'), Carbon::parse('2025-01-05')
+        Carbon::parse('2025-01-01'),
+        Carbon::parse('2025-01-05')
     );
     expect($reservation1->getReservationNights())->toBe(4);
 
     $reservation2 = new DateRange(
-        Carbon::parse('2025-01-10'), Carbon::parse('2025-01-13')
+        Carbon::parse('2025-01-10'),
+        Carbon::parse('2025-01-13')
     );
     expect($reservation2->getReservationNights())->toBe(3);
 
     $reservation3 = new DateRange(
-        Carbon::parse('2025-01-31'), Carbon::parse('2025-02-12')
+        Carbon::parse('2025-01-31'),
+        Carbon::parse('2025-02-12')
     );
     expect($reservation3->getReservationNights())->toBe(12);
+
+    $reservation4 = new DateRange(
+        Carbon::parse('2025-01-01'),
+        Carbon::parse('2026-01-02')
+    );
+    expect($reservation4->getReservationNights())->toBe(366);
 });
 
 it('should not allow reservation with a overlap date range', function () {
     $reservation1 = new DateRange(
-        Carbon::parse('2025-01-01'), Carbon::parse('2025-01-05')
+        Carbon::parse('2025-01-01'),
+        Carbon::parse('2025-01-05')
     );
     expect($reservation1->getReservationNights())->toBe(4);
 
     $reservation2 = new DateRange(
-        Carbon::parse('2025-01-02'), Carbon::parse('2025-01-13')
+        Carbon::parse('2025-01-02'),
+        Carbon::parse('2025-01-13')
     );
     $reservation3 = new DateRange(
-        Carbon::parse('2025-01-12'), Carbon::parse('2025-01-22')
+        Carbon::parse('2025-01-12'),
+        Carbon::parse('2025-01-22')
     );
     $reservation4 = new DateRange(
-        Carbon::parse('2025-01-22'), Carbon::parse('2025-01-24')
+        Carbon::parse('2025-01-22'),
+        Carbon::parse('2025-01-24')
     );
     expect($reservation1->overlaps($reservation2))->toBeTrue();
     expect($reservation2->overlaps($reservation3))->toBeTrue();
