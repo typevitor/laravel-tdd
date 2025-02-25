748ac79cadc013cda4cdccddfafc3fe6c067c385
Vitor Augusto <vitorlealeng@gmail.com>
2025-02-25 07:31:23 -0300

    refactor: update test group typo

--- tests/Unit/Application/EloquentUserTest.php
+++ tests/Unit/Application/EloquentUserTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class);
 
-describe('User Service', function () {
+describe('User Eloquent Service', function () {
 
     beforeEach(function () {
         $fakeUserRepository = new EloquentUserRepository();
