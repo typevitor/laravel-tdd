fd0774fea927d4e785eb9561917539e260700a4f
Vitor Augusto <vitorlealeng@gmail.com>
2025-03-05 08:26:42 -0300

    test: add test to create a new  booking

---
+++ tests/Feature/BookingTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Property;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create(['is_admin' => false]);
+    $this->property = Property::factory()->create();
+});
+
+test('should create a new booking', function () {
+    $booking = [
+        'property_id' => $this->property->id,
+        'user_id' => $this->user->id,
+        'start_date' => '2025-01-10',
+        'end_date' => '2025-01-15',
+        'occupants' => 2
+    ];
+    $this->post('/api/v1/bookings', $booking)
+        ->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => [
+                'property_id',
+                'user_id',
+                'occupants',
+            ],
+            'message'
+        ])
+        ->assertJsonFragment(['message' => 'Booking created successfully']);
+    $this->assertDatabaseHas('bookings', $booking);
+
+});
