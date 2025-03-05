7c88a32281e1e5587655d1ed84ef23cdceae3368
Vitor Augusto <vitorlealeng@gmail.com>
2025-03-05 08:33:17 -0300

    test: add tests to return error with invalid booking data

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -8,7 +8,7 @@ uses(RefreshDatabase::class);
 
 beforeEach(function () {
     $this->user = User::factory()->create(['is_admin' => false]);
-    $this->property = Property::factory()->create();
+    $this->property = Property::factory(['max_occupants' => 2])->create();
 });
 
 test('should create a new booking', function () {
@@ -32,4 +32,30 @@ test('should create a new booking', function () {
         ->assertJsonFragment(['message' => 'Booking created successfully']);
     $this->assertDatabaseHas('bookings', $booking);
 
+});
+
+test('should return error with an invalid date', function () {
+    $booking = [
+        'property_id' => $this->property->id,
+        'user_id' => $this->user->id,
+        'start_date' => 'invalid-date',
+        'end_date' => '2025-01-15',
+        'occupants' => 2
+    ];
+    $this->post('/api/v1/bookings', $booking)
+        ->assertStatus(400)
+        ->assertJsonStructure(['message']);
+});
+
+test('should return error with an start date higher than end date', function () {
+    $booking = [
+        'property_id' => $this->property->id,
+        'user_id' => $this->user->id,
+        'start_date' => '2025-01-20',
+        'end_date' => '2025-01-15',
+        'occupants' => 2
+    ];
+    $this->post('/api/v1/bookings', $booking)
+        ->assertStatus(422)
+        ->assertJsonStructure(['message']);
 });
